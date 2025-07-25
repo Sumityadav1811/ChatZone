d5dad0c2fbc9924838b99094662bae729ed00e9d
Sumit Kumar Yadav <12211053s@gmail.com>
2025-07-25 23:09:32 +0530

    messaeg

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // your React frontend
+    origin: "*", // your React frontend
     methods: ["GET", "POST"],
   },
 });
